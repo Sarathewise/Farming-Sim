7b5b412def61fa16269aa3c2bed1d941563f9f7f
Sarathewise <sarathewise124@gmail.com>
2025-04-16 22:52:49 -0400

    actual first commit

---
+++ game.js
@@ -0,0 +1,22 @@
+//Character Creation
+let playerName;
+let playerGender;
+let playerOrigin;
+let playerPrevJob;
+
+document.getElementById("submitButton").onclick = function() {
+    playerName = document.getElementById("Name").value
+    document.getElementById("playerName").textContent = playerName
+}
+
+const newGameButton = document.getElementById("newGameButton")
+const characterCreationCloseButton = document.getElementById("characterCreationCloseButton")
+const characterCreationScreen = document.getElementById("characterCreationScreen")
+
+newGameButton.addEventListener("click", () => {
+    characterCreationScreen.classList.add("show");
+});
+
+characterCreationCloseButton.addEventListener("click", () => {
+    characterCreationScreen.classList.remove("show");
+});
