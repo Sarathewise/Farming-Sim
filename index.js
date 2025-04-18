2d7cf4aa989f0ba207ff53093518194278cebc4d
Sarathewise <sarathewise124@gmail.com>
2025-04-17 23:48:37 -0400

    Working on button functionality and new game content

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
 console.log("Is this thing working?")
 
+//Modal Functionality
+
+const overlay = document.getElementById("overlay")
+const sidebarButtons = document.getElementsByClassName("sidebarButton")
+const closeButtons = document.getElementsByClassName("closeButton")
+const modal = document.getElementsByClassName("modal")
+
+function showModal() {
+    overlay.classList.add("show");
+    Array.from(modal).forEach((element) => element.classList.add("show"));
+}
+
+function hideModal() {
+    overlay.classList.remove("show");
+    Array.from(modal).forEach((element) => element.classList.remove("show"));
+}
+
+Array.from(closeButtons).forEach(element => {
+    element.addEventListener("click", () => {
+        hideModal();
+    });
+});
+
+//Sidebar Buttons
+
+Array.from(sidebarButtons).forEach(element => {
+    element.addEventListener("click", () => {
+        showModal();
+    });
+});
 
 //Character Creation
+
+const characterCreationScreen = document.getElementById("characterCreationScreen")
 let playerName;
 let playerGender;
 let playerOrigin;
 let playerPrevJob;
 
-document.getElementById("submitButton").onclick = function() {
-    playerName = document.getElementById("Name").value
-    document.getElementById("playerName").textContent = playerName
+//Saving the character creation form
+
+document.getElementById("characterCreation").addEventListener('submit', (event) => {
+    event.preventDefault()
+    form = document.getElementById("characterCreation") 
+    let formData =  new FormData(form)
+    console.log(formData)
+    // playerName = formData.playerName
+    playerName = formData.get("playerName")
+    document.getElementById("playerNameDisplay").textContent = playerName
+})
+
+function playerBio(playerName) {
+    let playerBio = `Your name is ${playerName}. You go by {playerPronouns} pronouns.`
+    document.getElementById("playerBio").textContent = playerBio
 }
-
-const overlay = document.getElementById("overlay")
-const newGameButton = document.getElementById("newGameButton")
-const characterCreationCloseButton = document.getElementById("characterCreationCloseButton")
-const characterCreationScreen = document.getElementById("characterCreationScreen")
-
-function showOverlay() {
-    overlay.classList.add("show");
-}
-
-function hideOverlay() {
-    overlay.classList.remove("show");
-}
-
-newGameButton.addEventListener("click", () => {
-    characterCreationScreen.classList.add("show");
-    showOverlay()
-});
-
-characterCreationCloseButton.addEventListener("click", () => {
-    characterCreationScreen.classList.remove("show");
-    hideOverlay()
-});
